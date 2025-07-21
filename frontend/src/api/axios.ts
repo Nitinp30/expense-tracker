f12ffeac73f953856b1127aea21749610dee91be
Nitinp30 <nitindevtech@gmail.com>
2025-07-21 18:19:35 +0530

    Update base url

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api", // ✅ your base URL
+  baseURL: "/api", // ✅ your base URL
 });
 
 api.interceptors.request.use((config) => {
